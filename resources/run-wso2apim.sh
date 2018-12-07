5a41c442509aeb077a733bb184969ef8c257f5b8
afilippov <afilippov@emdev.ru>
2018-12-08 00:26:19 +0300

    Minor changes

--- resources/run-wso2apim.sh
+++ resources/run-wso2apim.sh
@@ -7,6 +7,6 @@ echo '>>>JAVA_APP_DIR = ${JAVA_APP_DIR}'
 
 sed -ci.bak1 's|<!--HostName>www.wso2.org</HostName-->|<HostName>'"$WSO2_APIM_URL"'</HostName>|' $JAVA_APP_DIR/${WSO2_APIM_VERSION}/repository/conf/carbon.xml ; \
 sed -ci.bak1 's|<!--MgtHostName>mgt.wso2.org</MgtHostName-->|<MgtHostName>'"$WSO2_APIM_URL"'</MgtHostName>|' $JAVA_APP_DIR/${WSO2_APIM_VERSION}/repository/conf/carbon.xml
-sed -ci.bak1 's|redirectPort="9443"|redirectPort="443"|' $JAVA_APP_DIR/${WSO2_APIM_VERSION}/repository/conf/tomcat/catalina-server.xml
+sed -ci.bak1 's|port="9443"|port="9443" proxyPort="443"|' $JAVA_APP_DIR/${WSO2_APIM_VERSION}/repository/conf/tomcat/catalina-server.xml
 
 exec $JAVA_APP_DIR/$WSO2_APIM_VERSION/bin/wso2server.sh
