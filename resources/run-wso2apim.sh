2e06ded58e232109fdcf0121ff885e5a2356cdbf
afilippov <afilippov@emdev.ru>
2018-12-08 00:16:56 +0300

    Minor changes

--- resources/run-wso2apim.sh
+++ resources/run-wso2apim.sh
@@ -3,6 +3,7 @@
 set -ex
 
 echo '>>>WSO2_APIM_URL = ${WSO2_APIM_URL}'
+echo '>>>JAVA_APP_DIR = ${JAVA_APP_DIR}'
 
 sed -ci.bak1 's|<!--HostName>www.wso2.org</HostName-->|<HostName>'"$WSO2_APIM_URL"'</HostName>|' $JAVA_APP_DIR/${WSO2_APIM_VERSION}/repository/conf/carbon.xml ; \
 sed -ci.bak1 's|<!--MgtHostName>mgt.wso2.org</MgtHostName-->|<MgtHostName>'"$WSO2_APIM_URL"'</MgtHostName>|' $JAVA_APP_DIR/${WSO2_APIM_VERSION}/repository/conf/carbon.xml
