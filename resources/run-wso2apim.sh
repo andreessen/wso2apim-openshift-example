c24b0925dee221055c0495f4dcff7a32fbf1bc7b
afilippov <afilippov@emdev.ru>
2018-12-13 15:52:28 +0300

    python pip xq

--- resources/run-wso2apim.sh
+++ resources/run-wso2apim.sh
@@ -9,4 +9,5 @@ sed -ci.bak1 's|<!--HostName>www.wso2.org</HostName-->|<HostName>'"$WSO2_APIM_UR
 sed -ci.bak1 's|<!--MgtHostName>mgt.wso2.org</MgtHostName-->|<MgtHostName>'"$WSO2_APIM_URL"'</MgtHostName>|' $JAVA_APP_DIR/${WSO2_APIM_VERSION}/repository/conf/carbon.xml
 sed -ci.bak1 's|port="9443"|port="9443" proxyPort="443"|' $JAVA_APP_DIR/${WSO2_APIM_VERSION}/repository/conf/tomcat/catalina-server.xml
 
+
 exec $JAVA_APP_DIR/$WSO2_APIM_VERSION/bin/wso2server.sh
