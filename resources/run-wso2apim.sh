353a648c4cb2754735bd674d2a3c4ae2b9ea7354
afilippov <afilippov@emdev.ru>
2018-12-14 13:01:23 +0300

    New lines

--- resources/run-wso2apim.sh
+++ resources/run-wso2apim.sh
@@ -11,15 +11,15 @@ sed -ci.bak1 's|port="9443"|port="9443" proxyPort="443"|' $JAVA_APP_DIR/${WSO2_A
 
 mv $JAVA_APP_DIR/${WSO2_APIM_VERSION}/repository/conf/datasources/master-datasources.xml /tmp/master-datasources_.xml
 xsltproc /tmp/append_ds.xslt /tmp/master-datasources_.xml > /tmp/master-datasources.xml
-xsltproc --stringparam pApiMgtDBUrl jdbc:mysql://$MYSQL_SERVICE_HOST:3306/apimgtdb?autoReconnect=true
-	--stringparam pApiMgtDBPassword $MYSQL_PASSWORD --stringparam pApiMgtDBUser $MYSQL_USER
-	--stringparam pMBDBUrl jdbc:mysql://$MYSQL_SERVICE_HOST:3306/mbstoredb?autoReconnect=true
-	--stringparam pMBDBPassword $MYSQL_PASSWORD --stringparam pMBDBUser $MYSQL_USER
-	--stringparam pStatDBUrl jdbc:mysql://$MYSQL_SERVICE_HOST:3306/statdb?autoReconnect=true
-	--stringparam pStatDBPassword $MYSQL_PASSWORD --stringparam pStatDBUser $MYSQL_USER
-	--stringparam pRegDBUrl jdbc:mysql://$MYSQL_SERVICE_HOST:3306/regdb?autoReconnect=true
-	--stringparam pRegDBPassword $MYSQL_PASSWORD --stringparam pRegDBUser $MYSQL_USER
-	--stringparam pUMgtDBUrl jdbc:mysql://$MYSQL_SERVICE_HOST:3306/userdb?autoReconnect=true 
+xsltproc --stringparam pApiMgtDBUrl jdbc:mysql://$MYSQL_SERVICE_HOST:3306/apimgtdb?autoReconnect=true \
+	--stringparam pApiMgtDBPassword $MYSQL_PASSWORD --stringparam pApiMgtDBUser $MYSQL_USER \
+	--stringparam pMBDBUrl jdbc:mysql://$MYSQL_SERVICE_HOST:3306/mbstoredb?autoReconnect=true \
+	--stringparam pMBDBPassword $MYSQL_PASSWORD --stringparam pMBDBUser $MYSQL_USER \
+	--stringparam pStatDBUrl jdbc:mysql://$MYSQL_SERVICE_HOST:3306/statdb?autoReconnect=true \
+	--stringparam pStatDBPassword $MYSQL_PASSWORD --stringparam pStatDBUser $MYSQL_USER \
+	--stringparam pRegDBUrl jdbc:mysql://$MYSQL_SERVICE_HOST:3306/regdb?autoReconnect=true \
+	--stringparam pRegDBPassword $MYSQL_PASSWORD --stringparam pRegDBUser $MYSQL_USER \
+	--stringparam pUMgtDBUrl jdbc:mysql://$MYSQL_SERVICE_HOST:3306/userdb?autoReconnect=true  \
 	--stringparam pUMgtDBPassword $MYSQL_PASSWORD --stringparam pUMgtDBUser $MYSQL_USER master.xslt /tmp/master-datasources.xml > $JAVA_APP_DIR/${WSO2_APIM_VERSION}/repository/conf/datasources/master-datasources.xml
 
 mv $JAVA_APP_DIR/${WSO2_APIM_VERSION}/repository/conf/user-mgt.xml /tmp/user-mgt.xml
